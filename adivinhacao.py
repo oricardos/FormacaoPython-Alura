517c2a194296918c87db2e429383e7616cc547c7
oricardos <ricardo.rs95@gmail.com>
2023-02-02 23:33:41 -0300

    usando laco for

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,20 @@ print('**********************************')
 
 secret_number = 10
 
-attempt = int(input('Digite um número entre 1 e 10: '))
 
-if attempt > 10:
-    print('Só até 10 doido')
-elif attempt != secret_number:
-    print('Tente novamente!')
-else:
-    print('Acertou, miserável')
+
+total_attempt = 3
+
+for round_game in range(1, total_attempt + 11):
+    print('Total de tentativas: {} de {}'.format(round_game, total_attempt))
+    attempt = int(input('Digite um número entre 1 e 10: '))
+    
+    if attempt == secret_number:
+        print('acertou')
+    else:
+        if attempt > secret_number:
+            print('número maior')
+        elif attempt < secret_number:
+            print('número menor')
 
 print('Fim de Jogo')
