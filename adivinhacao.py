79cc7706be4e40caef70fc1d949db14ede6d64cf
oricardos <ricardo.rs95@gmail.com>
2023-06-16 23:23:41 -0300

    encerrando loop

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,20 +4,29 @@ print('**********************************')
 
 secret_number = 10
 
-
-
 total_attempt = 3
 
-for round_game in range(1, total_attempt + 11):
+for round_game in range(1, total_attempt + 1):
     print('Total de tentativas: {} de {}'.format(round_game, total_attempt))
     attempt = int(input('Digite um número entre 1 e 10: '))
+
+    if attempt < 1 or attempt > 100:
+        print("Você deve digitar um número entre 1 e 10")
+        continue
     
     if attempt == secret_number:
         print('acertou')
+        break
     else:
         if attempt > secret_number:
-            print('número maior')
+            print('**********************************')
+            print("Você digitou: ", attempt)
+            print('O número secreto é menor do que o número que você tentou')
+            print('**********************************')
         elif attempt < secret_number:
-            print('número menor')
+            print('**********************************')
+            print("Você digitou: ", attempt)
+            print('O número secreto é maior do que o número que você tentou')
+            print('**********************************')
 
 print('Fim de Jogo')
