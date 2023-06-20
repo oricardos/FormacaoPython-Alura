92dfc0e28f03b71070387f513ab852fcd9d816c3
oricardos <ricardo.rs95@gmail.com>
2023-06-19 22:59:23 -0300

    adicionando nivel ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,17 +5,28 @@ print('Bem vindo ao jogo de adivinhação!')
 print('**********************************')
 
 secret_number = random.randrange(0, 101)
+total_attempt = 0
 
-total_attempt = 3
+print('Escolha o nível do Jogo')
+print('(1) Fácil (2) Médio (3) Difícil')
+
+level = int(input('Selecione um nível: '))
+
+if level == 1:
+    total_attempt = 20
+elif level == 2:
+    total_attempt = 10
+else:
+    total_attempt = 5
 
 for round_game in range(1, total_attempt + 1):
     print('Total de tentativas: {} de {}'.format(round_game, total_attempt))
-    attempt = int(input('Digite um número entre 1 e 10: '))
+    attempt = int(input('Digite um número entre 1 e 100: '))
 
     if attempt < 1 or attempt > 100:
         print('**********************************')
         print("Você digitou: ", attempt)
-        print("Você deve digitar um número entre 1 e 10")
+        print("Você deve digitar um número entre 1 e 100")
         print('**********************************')
         continue
     
