9bc5b8e7e2f03ae77764e7b7cb8346c7ab122603
oricardos <ricardo.rs95@gmail.com>
2023-06-20 22:06:15 -0300

    add jogo da forca e criando menu de jogos

---
+++ games/games.py
@@ -0,0 +1,12 @@
+print('**************************************')
+print('*** Escolha qual jogo deseja jogar ***')
+print('**************************************')
+
+print('(1) - Forca | (2) - Adivinhação')
+
+game = int(input('Qual jogo  vai ser hoje?'))
+
+if game == 1:
+    print('*** Forca selecionado ***')
+else:
+    print('*** Adivinhação Selecionado ***')
